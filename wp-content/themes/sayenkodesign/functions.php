7c57186584b575679041372cdc6479d37f6ad56b
Robert M. Parker <rparker@yamiko.ninja>
2016-03-29 02:18:08 -0700

    referral widget

--- wp-content/themes/sayenkodesign/functions.php
+++ wp-content/themes/sayenkodesign/functions.php
@@ -33,7 +33,6 @@ add_action('admin_head', function () {
 HTML;
 });
 
-
 // login logo
 add_action('login_head', function() {
     $root_uri = get_template_directory_uri();
@@ -48,3 +47,24 @@ add_action('login_head', function() {
     </style>
 HTML;
 });
+
+
+// referral widget
+add_action( 'wp_dashboard_setup', function () {
+    wp_add_dashboard_widget(
+        'referral_dashboard_widget',
+        'RECEIVE $500 in CASH FOR A WEBSITE REFERRAL!!',
+        function() {
+            echo <<<HTML
+                <a href='http://www.sayenkodesign.com'>
+                    <img alt='Seattle Web Design' src='http://www.sayenkodesign.com/wp-content/uploads/2014/08/Sayenko-Design-WP-Referral-Bonus-460.jpg' width='100%'>
+                </a>
+                </br>
+                </br>
+                Simply introduce us via email along with the prospects phone number.
+                Email introductions can be sent to
+                <a href='mailto:mike@sayenkodesign.com'>mike@sayenkodesign.com</a>
+HTML;
+        }
+    );
+} );
